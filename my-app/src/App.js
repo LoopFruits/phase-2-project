a895577e41f9c4021ecc812fa107e565c99422b6
tylergubser <tylergubser2200@gmail.com>
2022-03-08 07:09:37 -0600

    updates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "../Header";
+import Footer from "../Footer";
+import Search from "../Search";
+import SelectionContainerList from "../SelectionContainerList";
+
 function App() {
   return (
     <div className="App">
