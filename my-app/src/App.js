20ad954716908d1132e10dd840065c8936f94bf6
Jane Kim <jane.kim0717@gmail.com>
2022-03-08 11:24:08 -0500

    Added App Name to Header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-// import Header from "./components/Header";
+import Header from "./components/Header";
 // import Footer from "./components/Footer";
 // import Search from "./components/Search";
 // import SelectionContainerList from "./components/SelectionContainerList";
@@ -8,20 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="image_Url" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-    </header>
+        <Header />
     </div>
   );
 }
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,22 +1,12 @@
 import React from "react";
-import Search from "./Search";
+// import Search from "./Search";
 function Header() {
 
 return (
     <header className="App-header">
-        <img src="image_Url" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Search/>
+        <h1>
+          Justin's Personal Vinyl Collection
+        </h1>
     </header>
 )
 
