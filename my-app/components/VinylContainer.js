530f387da3141029a48e22752c1036cdc79836ae
tylergubser <tylergubser2200@gmail.com>
2022-03-08 09:37:56 -0600

    fetched data

--- my-app/components/VinylContainer.js
+++ my-app/components/VinylContainer.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import VinylCard from "./VinylCard"
+
 
 function VinylContainer() {
+   const[vinyl, setVinyl] = useState([]) 
+    
+    
+    useEffect( () => {
+        fetch("http://localhost:3000/vinyls")
+        .then(resp => resp.json())
+        .then(item => setVinyl(item))
+    },[]);
+    
+    
+    
+    const inventoryList = vinyl.map((item) => {
+        return  <VinylCard key={item} item={item} />
+    })
 
 
-
-   
-
-
+    return(
+        <div id="current-inventory">
+            <h2>Current Inventory</h2>
+            <div>
+                {inventoryList}
+            </div>
+        </div>
+    );
 }
+
 
 export default VinylContainer;
