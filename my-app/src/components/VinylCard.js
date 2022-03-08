ff274dc667b728d0644471432256a7dfd04db3af
Jane Kim <jane.kim0717@gmail.com>
2022-03-08 11:48:04 -0500

    Added additional to Vinyl Card

--- my-app/src/components/VinylCard.js
+++ my-app/src/components/VinylCard.js
@@ -7,8 +7,10 @@ function VinylCard({ item }) {
 return (
     <div className="card" onClick={() => console.log("Clicking the item...")}>
             <img src={item.image} alt={item.album}></img>
-            <h3>{item.artist}</h3>
+            <h2>{item.artist}</h2>
             <h4>${item.price}</h4>
+            <h3>Reviews: <br/>{item.reviews}<br/> </h3>
+            <h4>Release Date: {item.releasedate}</h4>
             <button>Remove</button>
         </div>
 )
