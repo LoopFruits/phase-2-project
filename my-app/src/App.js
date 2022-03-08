0c3b19e48495d77faf5e85f4be8ef2f645922db7
LoopFruits <Kensworldc@gmail.com>
2022-03-08 17:11:35 -0500

    moved SelectionContainerList above VinylContainer in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,8 +10,9 @@ function App() {
   return (
     <div className="App">
         <Header />
-        <VinylContainer />
         <SelectionContainerList />
+        <VinylContainer />
+        
     </div>
   );
 }
