35223a9b69e11f06b10d620d438d6a1ced59d446
tylergubser <tylergubser2200@gmail.com>
2022-03-08 10:15:54 -0600

    put header back to original

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Search from "./components/Search";
-import SelectionContainerList from "./components/SelectionContainerList";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Search from "./components/Search";
+// import SelectionContainerList from "./components/SelectionContainerList";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <header className="App-header">
+        <img src="image_Url" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </header>
     </div>
   );
 }
