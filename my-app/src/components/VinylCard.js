f8dd9b52ef07057f513d1bfcfde3a4c0ec4b636f
LoopFruits <Kensworldc@gmail.com>
2022-03-09 10:14:41 -0500

    added album name to card

--- my-app/src/components/VinylCard.js
+++ my-app/src/components/VinylCard.js
@@ -8,6 +8,7 @@ return (
     <div className="card" onClick={() => console.log("Clicking the item...")}>
             <img src={item.image} alt={item.album}></img>
             <h2>{item.artist}</h2>
+            <h2>{item.album}</h2>
             <h4>${item.price}</h4>
             <h3>Reviews: <br/>{item.reviews}<br/> </h3>
             <h4>Release Date: {item.releasedate}</h4>
